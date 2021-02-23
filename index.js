baeff911113d32400d2f962809fc094c0c366f2f
QuenMBar <Qbeast360@gmail.com>
2021-02-22 17:24:50 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Quentin Barnes";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
